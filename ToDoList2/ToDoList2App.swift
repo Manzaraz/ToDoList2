15eb43fac75bd851bfc753f3dd8bcc6e8c7d5daa
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-07 21:35:05 -0300

    Initial Commit

---
+++ ToDoList2/ToDoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2App.swift
+//  ToDoList2
+//
+//  Created by Christian Manzaraz on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
