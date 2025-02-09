212e020491c8ea393146871dd3be7020a7d65cba
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-09 13:54:40 -0300

    Ch. 5.4 A Beginner's Guide to Classes in Swift (To Do List) 2024

--- ToDoList2/DetailView.swift
+++ ToDoList2/DetailView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct DetailView: View {
-    @State var toDo: String // Don't initialize it - It will be passed from the parent view
+    @State var toDo: ToDo
+    @State private var item =  ""
     @State private var reminderIsOn = false
-//    @State private var dueDate = Date.now + 60*60*24
+    //    @State private var dueDate = Date.now + 60*60*24
     @State private var dueDate = Calendar.current.date(
         byAdding: .day,
         value: 1,
@@ -19,11 +21,13 @@ struct DetailView: View {
     @State private var notes = ""
     @State private var isCompleted = false
     
+    @Environment(\.modelContext) var modelContect
+    
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         List {
-            TextField("Enter To Do here...", text: $toDo)
+            TextField("Enter To Do here...", text: $item)
                 .font(.title)
                 .textFieldStyle(.roundedBorder)
                 .padding(.vertical)
@@ -37,10 +41,10 @@ struct DetailView: View {
                 .listRowSeparator(.hidden)
                 .padding(.bottom)
                 .disabled(!reminderIsOn)
-                
+            
             Text("Notes:")
                 .padding(.top)
-
+            
             TextField("Notes...", text: $notes, axis: .vertical)
                 .textFieldStyle(.roundedBorder)
                 .listRowSeparator(.hidden)
@@ -48,9 +52,17 @@ struct DetailView: View {
             Toggle("Set Reminder:", isOn: $isCompleted)
                 .padding(.top)
                 .listRowSeparator(.hidden)
-
+            
         }
         .listStyle(.plain)
+        .onAppear() {
+            // Move data from toDo object to local vars
+            item = toDo.item
+            reminderIsOn = toDo.reminderIsOn
+            dueDate = toDo.dueDate
+            notes = toDo.notes
+            isCompleted = toDo.isCompleted
+        }
         .navigationBarBackButtonHidden()
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
@@ -61,18 +73,31 @@ struct DetailView: View {
             
             ToolbarItem(placement: .topBarTrailing) {
                 Button("Save") {
-                    // Add Save Code Here
+                    // Move data from local vars to toDo object
+                    toDo.item = item
+                    toDo.reminderIsOn = reminderIsOn
+                    toDo.dueDate = dueDate
+                    toDo.notes = notes
+                    toDo.isCompleted = isCompleted
+                    
+                    modelContect.insert(toDo)
+                    guard let _ = try? modelContect.save() else {
+                        print("🤬 Error: Save on DetailView did not work.")
+                        return
+                    }
+
+                    dismiss()
                 }
             }
             
         }
-
+        
     }
 }
 
 #Preview {
     NavigationStack {
-        DetailView(toDo: "")
-        
+        DetailView(toDo: ToDo())
+            .modelContainer(for: ToDo.self, inMemory: true)
     }
 }
